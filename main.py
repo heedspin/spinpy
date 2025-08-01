62d9dceb9b9e37df3bca9c7527eac60053dc99e5
Tim Harrison <heedspin@gmail.com>
2025-08-01 10:30:04 +0000

    Assume access to a data folder.  Need to figure out how to mount it.

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from ipdb import set_trace as st
 from urllib.request import urlretrieve 
 import pandas as pd
 import matplotlib
-matplotlib.use('Agg')  # Use non-interactive backend
+# matplotlib.use('Agg')  # Use non-interactive backend
 import matplotlib.pyplot as plt
 url = "https://raw.githubusercontent.com/MolSSI-Education/molssicheminfo/refs/heads/master/data/PubChemElements_all.csv"
-filename = "tmp/PubChemElements_all.csv"
+filename = "data/PubChemElements_all.csv"
 urlretrieve(url, filename)
+print(f"Downloaded data to {filename}")
 df = pd.read_csv(filename)
 
 subplot = df.hist(figsize=(8, 8), edgecolor="black", grid=False)
-plt.savefig('tmp/histogram.png', dpi=300, bbox_inches='tight')
-
+plt.savefig('data/histogram.png', dpi=300, bbox_inches='tight')
+print("Histogram saved as data/histogram.png")
+plt.close()  # Close the plot to free memory
+exit(0)
